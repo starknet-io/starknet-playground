0ba516dec4e11a3c2dac627d5f9f420596951667
Carmit Kleinik <carmit@starkware.co>
2022-01-21 07:57:41 +0200

    enable editing tabs' names

--- src/components/UI/NavItem/NavItem.js
+++ src/components/UI/NavItem/NavItem.js
@@ -1,16 +1,54 @@
 import PropTypes from 'prop-types';
-import React from 'react';
+import React, {useState} from 'react';
 import {ReactComponent as XIcon} from '../../../assets/svg/x.svg';
 import styles from './NavItem.module.scss';
 
-const NavItem = ({text, isActive, onClick, onDelete}) => {
-  const classes = [styles.navItem, isActive && styles.isActive].join(
-    ' '
-  );
+const NavItem = ({
+  text,
+  isActive,
+  isEditable,
+  onBlur,
+  onClick,
+  onDelete
+}) => {
+  const [tabNameInput, setTabNameInput] = useState(text);
+  const classes = [
+    styles.navItem,
+    isActive && styles.isActive,
+    isEditable && styles.isEditable
+  ].join(' ');
+  const handleKeyPress = event => {
+    if (event?.key === 'Enter') {
+      onBlur(tabNameInput);
+    }
+  };
+  const editingStateStyle = {
+    maxWidth: 200
+  };
+  const viewingStateStyle = {
+    maxWidth: 142
+  };
+  const getInputSize = () => {
+    return tabNameInput?.length
+      ? (tabNameInput.length * 0.75).toFixed(0)
+      : 5;
+  };
 
   return (
     <button className={classes} onClick={onClick}>
-      {text}
+      <div>
+        <input
+          autoFocus
+          maxLength={30}
+          readOnly={!isEditable}
+          size={getInputSize()}
+          style={isEditable ? editingStateStyle : viewingStateStyle}
+          value={tabNameInput}
+          onBlur={() => onBlur(tabNameInput)}
+          onChange={e => setTabNameInput(e.target.value)}
+          onKeyPress={e => handleKeyPress(e)}
+        />
+      </div>
       <XIcon onClick={onDelete} />
     </button>
   );
@@ -19,6 +57,8 @@ const NavItem = ({text, isActive, onClick, onDelete}) => {
 NavItem.propTypes = {
   text: PropTypes.string,
   isActive: PropTypes.bool,
+  isEditable: PropTypes.bool,
+  onBlur: PropTypes.func,
   onClick: PropTypes.func,
   onDelete: PropTypes.func
 };
